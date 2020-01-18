2cb99def78fdd43a58e91234571a7fa94140adbe
Matthew Jagiello <mjagiello@gmail.com>
2020-01-18 07:09:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   new_numbers = []
   counter = 0
   while counter < array.length do
-    new_numbers.unshift (array[counter] * array[counter]
+    new_numbers.unshift (array[counter] * array[counter])
   end
 end
